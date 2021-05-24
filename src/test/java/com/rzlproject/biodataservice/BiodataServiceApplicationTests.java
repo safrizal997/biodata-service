d0a43f7562c13d1e03057b72ab9b6e42de44bd65
Safrizal <axrizal092@gmail.com>
2021-05-24 15:59:10 +0700

    add asset

---
+++ src/test/java/com/rzlproject/biodataservice/BiodataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rzlproject.biodataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiodataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
